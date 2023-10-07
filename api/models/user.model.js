e6a68b6f86223517a08c31baa859c07d8da1c572
Rayan-Weragala <110955943+Rayan-Weragala@users.noreply.github.com>
2023-10-07 22:54:06 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const userSchema =new mongoose.Schema({
+    firstname:{
+        type:String,
+    },
+    lastname:{
+        type:String,
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    address:{
+        type:String,
+    },
+    mobile:{
+        type:String,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+},{timeStamps:true});
+
+const User = mongoose.model('User',userSchema);
+
+export default User;
