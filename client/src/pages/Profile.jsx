f4c5695386f2e5ec4c2805f8675482cd06d17ba3
Rayan-Weragala <110955943+Rayan-Weragala@users.noreply.github.com>
2023-10-08 18:53:05 +0530

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import {useSelector,useDispatch} from 'react-redux'
 import {useRef, useState,useEffect} from 'react'
 import {getDownloadURL, getStorage,ref, uploadBytesResumable} from 'firebase/storage'
 import { app } from '../firebase';
-import {updateUserStart,updateUserFailure,updateUserSuccess} from '../redux/user/userSlice'
+import {updateUserStart,updateUserFailure,updateUserSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess} from '../redux/user/userSlice'
 
 export default function Profile() {
   const fileRef =useRef(null);
@@ -80,7 +80,24 @@ const handleSubmit =async (e) =>{
   }
 }
 
-
+const handleDeleteUser = async () =>{
+  try{
+    dispatch(deleteUserStart());
+    const res =await fetch(`/api/user/delete/${curruntUser._id}`,{
+      method:'delete',
+    })
+    const data =await res.json();
+    if(data.success===false){
+      dispatch(deleteUserFailure(data.message));
+      return;
+    }
+    dispatch(deleteUserSuccess(data))
+  
+  }catch(error){
+    dispatch(deleteUserFailure(error.message))
+   
+}
+}
   return (
         <div className="max-w-lg mx-auto bg-white p-6 rounded-md shadow-md">
           <h2 className="text-3xl font-semibold text-center
@@ -201,8 +218,8 @@ const handleSubmit =async (e) =>{
                 </div>
              </form>
              <div className='flex justify-between mt-5'>
-              <span className='text-red-700  cursor-pointer'>Delete Account</span>
-              <span className='text-red-700 cursor-pointer'>Sign out</span>
+              <span onClick={handleDeleteUser} className='text-black  hover:font-bold  cursor-pointer'>Delete Account</span>
+              <span className='text-black  hover:font-bold cursor-pointer'>Sign out</span>
 
              </div>
              <p className='text-red-700 mt-5'>{error ? error: ''}</p>
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -33,7 +33,19 @@ const userSlice = createSlice({
             state.error =action.payload;
             state.loading=false;
         },
+        deleteUserStart:(state)=>{
+            state.loading=true;
+        },
+        deleteUserSuccess:(state)=>{
+            state.curruntUser =null
+            state.loading=false;
+            state.error=null;
+        },
+        deleteUserFailure:(state,action)=>{
+            state.error =action.payload;
+            state.loading=false;
+        },
     }
 });
-export const {signInFailure,signInSuccess,signInStart,updateUserFailure,updateUserStart,updateUserSuccess} =userSlice.actions;
+export const {signInFailure,signInSuccess,signInStart,updateUserFailure,updateUserStart,updateUserSuccess,deleteUserFailure,deleteUserStart,deleteUserSuccess} =userSlice.actions;
 export default userSlice.reducer;
