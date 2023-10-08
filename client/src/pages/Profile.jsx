9284aa55cec4622c35fbd98053867a8fe95aa4df
Rayan-Weragala <110955943+Rayan-Weragala@users.noreply.github.com>
2023-10-08 14:50:26 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,84 @@
 import {useSelector} from 'react-redux'
+import {useRef, useState,useEffect} from 'react'
+import {getDownloadURL, getStorage,ref, uploadBytesResumable} from 'firebase/storage'
+import { app } from '../firebase';
 
 export default function Profile() {
+  const fileRef =useRef(null);
   const {curruntUser} =useSelector((state)=>state.user)
+  const [file,setFile] = useState(undefined);
+  const [filePerc,setFilePerc] =useState(0);
+  const [fileUploadError,setFileUploadError] =useState(false);
+  const [formData,setFormData] =useState({});
+  console.log(fileUploadError);
+//firebase storage 
+//allow read;
+//allow write:if
+//request.resource.size <2 * 1024 * 1024 && 
+//request.resource.contentType.matches('image/.*')
+ 
+useEffect(()=>{
+  if(file){
+    handleFileUpload(file)
+  }
+},[file]);
+const handleFileUpload =(file) =>{
+  const storage = getStorage(app);
+  const fileName = new Date().getTime()+file.name;
+  const storageRef = ref(storage,fileName)
+  const uploadTask = uploadBytesResumable(storageRef,file);
+
+  uploadTask.on('state_changed',
+  (snapshot)=>{
+    const progress = (snapshot.bytesTransferred /
+    snapshot.totalBytes) * 100;
+    setFilePerc(Math.round(progress))
+  },
+ 
+  (error)=>{
+    setFileUploadError(true)
+  },
+  ()=>{
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=> 
+    setFormData({...formData,avatar:downloadURL})
+    );
+  }
+  );
+};
   return (
-    <div className="max-w-lg mx-auto bg-white p-6 rounded-md shadow-md">
-      <h2 className="text-3xl font-semibold text-center
-      my-3">My Profile</h2>
-      <form className='flex flex-col'>
-        <img src={curruntUser.avatar} alt=''
-        className='rounded-full h-24 w-24 object-cover
-        cursor-pointer self-center mt-2'
-        />
+        <div className="max-w-lg mx-auto bg-white p-6 rounded-md shadow-md">
+          <h2 className="text-3xl font-semibold text-center
+          my-3">My Profile</h2>
+          <form className='flex flex-col'>
+                <input 
+                onChange={(e)=>setFile(e.target.files[0])} 
+                type='file' 
+                ref={fileRef} 
+                hidden 
+                accept='image/*' />
+
+              <img 
+                onClick={()=>fileRef.current.click()} 
+                src={formData.avatar || curruntUser.avatar} 
+                alt=''
+                className='rounded-full h-24 w-24 object-cover
+                cursor-pointer self-center mt-2'
+                />
+                <p className='text-sm self-center'>
+                  {fileUploadError ? (
+                  <span className='text-red-700'>Error Image Upload
+                  (Image must be less than 2 mb)
+                  </span>
+                    ) : filePerc >0 && filePerc <100 ? (
+                      <span className='text-slate-700'>{`Uploading 
+                      ${filePerc}%`} </span>
+                    ) : filePerc ===100 ? (
+                        <span className='text-green-700'>Image 
+                        successfully uploaded!</span>
+                     ) : (
+                      ''
+                      ) }
+                </p>
                 <div className="mb-4 flex mt-5">
                     <div className="w-1/2 mr-2">
                         <label htmlFor="firstname" className="block text-md font-medium">First Name</label>
